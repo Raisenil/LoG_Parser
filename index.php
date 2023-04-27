5c6c3f487161488423c516e56942594fa743f389
Raise nil <raisulislamniloy47@gmail.com>
2023-04-27 10:26:26 +0600

    index page added

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+
+<div class="bgimg">
+    <div class="middle">
+        <a href="View/LogView.php" class="btn btn-secondary btn-lg" style="margin-top: 300%;">Purge</a>
+    </div>
+</div>
+
+</body>
+</html>
